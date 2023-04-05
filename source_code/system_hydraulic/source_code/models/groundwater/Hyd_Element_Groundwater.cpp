afee40dcb0063a094fa0630887308bff51d614cc
dabachma <daniel.bachmann@h2.de>
2023-04-05 09:11:15 +0200

    test

---
+++ source_code/system_hydraulic/source_code/models/groundwater/Hyd_Element_Groundwater.cpp
@@ -0,0 +1,11 @@
+#include "source_code\Hyd_Headers_Precompiled.h"
+#include "Hyd_Element_Groundwater.h"
+
+Hyd_Element_Groundwaterr::Hyd_Model_Groundwater()
+{
+}
+
+
+Hyd_Model_Groundwater::~Hyd_Model_Groundwater()
+{
+}
---
+++ source_code/system_hydraulic/source_code/models/groundwater/Hyd_Element_Groundwater.h
@@ -0,0 +1,9 @@
+#pragma once
+class Hyd_Element_Groundwater
+{
+public:
+	///Default constructor
+	Hyd_Element_Groundwater(void);
+	~Hyd_Element_Groundwater();
+};
+
